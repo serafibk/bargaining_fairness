5502bd2d287bee737221872a8ec7d72283759918
Serafina Kamp <serafibk@serafina-mba.local>
2024-10-25 18:09:28 -0400

    correlated equilibria addition

--- correlated_equilibria.py
+++ correlated_equilibria.py
@@ -2,7 +2,7 @@ from scipy.optimize import linprog
 import numpy as np
 
 # shared possible actions
-S_i = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+S_i = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
 joint_strategies = [(a_i,a_j) for a_i in S_i for a_j in S_i]
        
 # payoff matrices
@@ -43,23 +43,29 @@ for j_a in range(len(S_i)):
 
 constraint_ub = [0 for i in range(np.shape(constraint_matrix)[0])] # upper bound of each constraint is 0
 
+
+# TODO -- add some constraints for not pure and not mixed (also just use gaussian elimination? - no that's for equalities)
+
 # probability constraints
 prob_constraint = [[1 for i in range(len(S_i)*len(S_i))]] # sum to 1
 p_ij_bounds = (0,1) # valid probability values
 
 
 # (negative) objective function try a range of objective functions
-for a_i in range(len(S_i)):
-    for a_j in range(len(S_i)):
-        social_welfare = [-1 if i == a_i and j == a_j else 0 for i in range(len(S_i)) for j in range(len(S_i))] # maximize probabilities of each possible combination
+# for a_i in range(len(S_i)):
+#     for a_j in range(len(S_i)):
+social_welfare = [-1 if (i == 6 and j == 7) or (i == 5 and j == 6) else 0 for i in range(len(S_i)) for j in range(len(S_i))] # maximize probabilities of each possible combination
+social_welfare[7*len(S_i)+8] =  -1
+print(social_welfare)
 
-        result = linprog(social_welfare, A_ub=constraint_matrix, b_ub=constraint_ub, A_eq=prob_constraint, b_eq=1, bounds=[p_ij_bounds for i in range(len(S_i)*len(S_i))])
-        print(f"A_i = {S_i[a_i]}, A_j = {S_i[a_j]}")
-        print(result.fun)
-        print(result.message)
-        for i,p in enumerate(result.x):
-            if p > 0:
-                print(f"Strategy: {joint_strategies[i]}, probability: {p}")
+result = linprog(social_welfare, A_ub=constraint_matrix, b_ub=constraint_ub, A_eq=prob_constraint, b_eq=1, bounds=[p_ij_bounds for i in range(len(S_i)*len(S_i))])
+# print(f"A_i = {S_i[a_i]}, A_j = {S_i[a_j]}")
+print(result.fun)
+print(result.message)
+for i,p in enumerate(result.x):
+    if p > 0:
+        print(f"Strategy: {joint_strategies[i]}, probability: {p}")
+print()
         # print(result.x)
         
 
